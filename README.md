f8a1bf17de6f6532820f6b61391bd116698b63ff
张晔 <y@qilephp.com>
2020-04-16 11:28:28 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# laravel-red-packet-compute
+
---
+++ src/RedPacketComputeServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Qihucms;
+
+use Illuminate\Support\ServiceProvider;
+
+class RedPacketComputeServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->singleton(RedPacketCompute::class, function () {
+            return new RedPacketCompute();
+        });
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
